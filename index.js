8a8b1ad2d3e641097cc1573ac8619759f60a874f
TejasAdhikari <tejasadhikari123@gmail.com>
2020-05-20 18:37:13 +0530

    Dice Game commit 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor(((Math.random())*6))+1;
-document.querySelector(".container .dice .img1").setAttribute("src","C:/Users/Lenovo/Desktop/T/web development/Dicee Challenge - Starting Files/images/dice"+randomNumber1+".png");
+document.querySelector(".container .dice .img1").setAttribute("src","images/dice"+randomNumber1+".png");
 var randomNumber2 = Math.floor(((Math.random())*6))+1;
-document.querySelector(".container .dice .img2").setAttribute("src","C:/Users/Lenovo/Desktop/T/web development/Dicee Challenge - Starting Files/images/dice"+randomNumber2+".png");
+document.querySelector(".container .dice .img2").setAttribute("src","images/dice"+randomNumber2+".png");
 if(randomNumber1===randomNumber2){
   document.querySelector(".container h1").innerHTML = "Draw!";
 }
